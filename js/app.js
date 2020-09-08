f3e8fba9a1c3befbe4b7da0c316d78c9eb10b506
Code_Zealot (Tyler W. Phass) <tyler@codezealot.com>
2020-09-07 22:33:06 -0400

    started adding code to modules

--- js/app.js
+++ js/app.js
@@ -8,10 +8,49 @@ var budgetController = (function() {
 // UI Controller Module
 var UIController = (function() {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+        
+    }
+    return {
+        getInput: function(){
+          return {
+            type: document.querySelector(DOMstrings.inputType).value, //inc or exp depending on selection
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value 
+          }
+        }
+        
+        getDOMStrings: function(){
+            return DOMstrings;
+        }
+    };
+    
 })();
 
 
 // App Controller Module
 var controller = (function(budgetCtrl, UICtrl) {
+    
+    var DOM = UICtrl.getDOMStrings;
+    
+    var ctrlAddItem = function() {
+        var input = UICtrl.getInput;
+    }
+    
+    document.querySelector(DOM.inputBtn)).addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event){
+        
+        if (event.keyCode === 13 || event.which === 13){
+            //event.which for older browsers that do not have event.keyCode prop
+            ctrlAddItem();
+        }
+        
+    });
+    
     
 })(budgetController,UIController);
